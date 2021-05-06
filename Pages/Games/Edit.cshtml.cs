b6e93e22e10208cfbf1b035c4dbb35d20a81b8fa
Rees Byrd <rtbyrd1@buffs.wtamu.edu>
2021-05-06 15:40:24 -0500

    Push#10

---
+++ Pages/Games/Edit.cshtml.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FinalProjectGameWebsite.Models;
+
+namespace FinalProjectGameWebsite.Pages.Games
+{
+    public class EditModel : PageModel
+    {
+        private readonly FinalProjectGameWebsite.Models.Context _context;
+
+        public EditModel(FinalProjectGameWebsite.Models.Context context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Game Game {get; set;}
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Game = await _context.Games.FirstOrDefaultAsync(g => g.GameId == id);
+
+            if (Game == null)
+            {
+                return NotFound();
+            }
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Game).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GameExists(Game.GameId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool GameExists(int id)
+        {
+            return _context.Games.Any(e => e.GameId == id);
+        }
+    }
+}
